6ce7f38daa9ceae0db9df3bf84ddad193bc249b0
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-03-31 09:44:18 +0300

    Init

---
+++ lab_8.2/lab_8.2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string findWord(string, char);
+
+int main()
+{
+  string str;
+
+  cout << "Enter string:" << endl;
+  getline(cin, str);
+
+  cout << "Last word (starts with \"a\"): " << findWord(str, 'a') << endl;
+
+  return 0;
+}
+
+string findWord(string str, char letter)
+{
+ if (str.find(letter) == -1)
+    return "";
+    
+ string word = "";
+
+  if (str.find(' ') == -1)
+    word = str;
+
+  size_t pos = 0;
+
+  while ((pos = str.find(' ', pos)) != -1) {
+    if (str[pos + 1] == letter)
+      word.assign(str, pos + 1, str.find(' ', pos + 1) - pos - 1);
+    pos++;
+  }
+
+  return word;
+}
